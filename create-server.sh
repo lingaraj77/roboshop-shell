c88b045da0f7a9d927d52205cb235f62952511fc
lingaraj77 <lingarajumodhala123@gmail.com>
2023-09-08 18:35:01 -0400

    ansible

--- create-server.sh
+++ create-server.sh
@@ -6,7 +6,6 @@ IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-044145deea7316391
 DOMAIN_NAME=joindevops.blog
 HOSTED_ZONE_ID=Z05137822OZWEMAN9Q55F
-SUBNET_ID=subnet-039bea7f7f5129e02
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -19,7 +18,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
