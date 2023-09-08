ec563e3ecb294a0b8d72bac438b882fc5371798d
lingaraj77 <lingarajumodhala123@gmail.com>
2023-09-08 19:02:20 -0400

    ansible

--- create-server.sh
+++ create-server.sh
@@ -5,8 +5,6 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-044145deea7316391
 DOMAIN_NAME=joindevops.blog
-HOSTED_ZONE_ID=Z05137822OZWEMAN9Q55F
-SUBNET_ID=subnet-039bea7f7f5129e02
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -19,10 +17,10 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet_id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i SUBNET_IDinstance: $IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
@@ -38,4 +36,4 @@ done
 
 # imporvement
 # check instance is already created or not
-# check route53 record is already exist, if exist update, otherwise create route53 record
+# update route53 record
